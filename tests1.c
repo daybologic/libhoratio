a181922bdf426476a6083f039630ad03be5967e5
David Duncan Ross Palmer <palmer@overchat.org>
2012-09-15 11:57:33 +0100

    Add initial empty test suite framework (CUnit)

---
+++ tests1.c
@@ -0,0 +1,236 @@
+/*
+*/
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /*HAVE_CONFIG_H*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "CUnit/Basic.h"
+#ifdef HDRSTOP
+# pragma hdrstop
+#endif /*HDRSTOP*/
+
+#include "dpcrtlmm.h"
+
+#define DIE(m) Die((__FILE__), (__LINE__), (m))
+
+enum testResultType {
+	TEST_FAIL = 1,
+	TEST_OK = 0
+};
+/*-------------------------------------------------------------------------*/
+int main(int argc, char *argv[]);
+
+/* Command-line argument processing */
+static bool ProcessOptions(int ArgC, char **ArgV);
+
+/* Suite initialisation routines */
+static int init_suite_core(void); /* Core library implementation testing: dpcrtlmm.c  */
+static int init_suite_trap(void); /* Trap function testing: dpc_trap.c */
+
+/* Suite cleanup routines */
+static int clean_suite_core(void);
+static int clean_suite_trap(void);
+
+/* This function aborts the program under extraordinary circumstances */
+static void Die(const char *File, const unsigned int Line, const char *Message);
+
+/* Simple support for concatinating short sequences of strings in tests,
+you should call Unglue() as soon as the tests are finished */
+static const char *Glue(const char *Str1, const char *Str2);
+static void Unglue(void);
+
+/* Test suite core */
+static void suite_core_Ver(void);
+
+/* Test suite trap */
+static void suite_trap_InstallTrapCallback(void);
+/*-------------------------------------------------------------------------*/
+static char *GlueStrs[10]; /* A small cache used by the Glue() and Unglue() functions */
+static bool SandboxStarted = false;
+static unsigned short int DebugLevel = 0U;
+/*-------------------------------------------------------------------------*/
+static bool ProcessOptions(int ArgC, char **ArgV)
+{
+	int c;
+	opterr = 0;
+
+	while ( (c = getopt(ArgC, ArgV, "d:")) != -1 ) {
+		switch ( c ) {
+			case 'd' : {
+				DebugLevel = (unsigned int)(abs(atoi(optarg)));
+				break;
+			}
+			default : {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+/*-------------------------------------------------------------------------*/
+static int init_suite_core()
+{
+	return 0;
+}
+/*-------------------------------------------------------------------------*/
+static int init_suite_trap()
+{
+	return 0;
+}
+/*-------------------------------------------------------------------------*/
+static int clean_suite_core()
+{
+	return 0;
+}
+/*-------------------------------------------------------------------------*/
+static int clean_suite_trap()
+{
+	return 0;
+}
+/*-------------------------------------------------------------------------*/
+static void Die(const char *File, const unsigned int Line, const char *Message)
+{
+	unsigned int l = strlen(Message);
+	if ( l && l-1 == '\n' ) {
+		fprintf(stderr, "%s", Message);
+	} else {
+		fprintf(stderr, "%s at line %u in %s\n", File, Line, Message);
+	}
+	abort();
+}
+/*-------------------------------------------------------------------------*/
+static const char *Glue(const char *Str1, const char *Str2)
+{
+	static const char empty[] = "";
+	size_t i;
+	int loc = -1;
+	size_t c = sizeof(GlueStrs)/sizeof(GlueStrs[0]);
+	for ( i = 0U; i < c; i++ ) {
+		if ( !GlueStrs[i] ) {
+			size_t allocSize = strlen(Str1)+strlen(Str2)+1;
+			GlueStrs[i] = malloc(allocSize);
+			if ( GlueStrs[i] ) {
+				strcpy(GlueStrs[i], Str1);
+				strcat(GlueStrs[i], Str2);
+				loc = i;
+				break;
+			} else {
+				fprintf(stderr, "Glue() malloc(%u) failed at %s L%u\n", (unsigned int)allocSize, __FILE__, __LINE__);
+				return empty;
+			}
+		}
+	}
+
+	if ( loc == -1 ) {
+		fprintf(stderr, "Glue() all %u temporary slots exhausted at %s L%u\n", (unsigned int)c, __FILE__, __LINE__);
+		return empty;
+	}
+	return GlueStrs[loc];
+}
+/*-------------------------------------------------------------------------*/
+static void Unglue()
+{
+	size_t i;
+	for ( i = 0U; i < sizeof(GlueStrs)/sizeof(GlueStrs[0]); i++ ) {
+		if ( GlueStrs[i] ) {
+			free(GlueStrs[i]);
+			GlueStrs[i] = NULL;
+		} else {
+			break;
+		}
+	}
+}
+/*-------------------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+	CU_ErrorCode err;
+	unsigned int failCount;
+	size_t testI, suiteI;
+	static struct {
+		const char *TestName;
+		void (*TestFunc)(void);
+	} CoreTests[] = {
+		{ "Ver",
+		  &suite_core_Ver
+		}
+	};
+	static struct {
+		const char *TestName;
+		void (*TestFunc)(void);
+	} TrapTests[] = {
+		{ "TrapInstallTrapCallback",
+		  &suite_trap_InstallTrapCallback
+		}
+	};
+
+	/* Suites */
+	static struct {
+		const char *SuiteName;
+		int (*Init)(void);
+		int (*Clean)(void);
+	} Suites[] = {
+		{ "suite_core", &init_suite_core, &clean_suite_core },
+		{ "suite_trap", &init_suite_trap, &clean_suite_trap }
+	};
+
+	CU_pSuite pSuite[] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+
+	if ( !ProcessOptions(argc, argv) ) return EXIT_FAILURE; /* Bail, if command-line arguments are bad */
+
+	/* initialize the CUnit test registry */
+	if ( CU_initialize_registry() != CUE_SUCCESS )
+		return CU_get_error();
+
+	/* Add all suites to the registry*/
+	for ( suiteI = 0U; suiteI < sizeof(Suites)/sizeof(Suites[0]); suiteI++ ) {
+		pSuite[suiteI] = CU_add_suite(Suites[suiteI].SuiteName, Suites[suiteI].Init, Suites[suiteI].Clean);
+		if ( !pSuite[suiteI] ) {
+			CU_cleanup_registry();
+			return CU_get_error();
+		}
+	}
+	suiteI = 0U;
+
+	/* add the tests to the suite */
+	for ( testI = 0; testI < sizeof(CoreTests)/sizeof(CoreTests[0]); testI++ ) {
+		if ( !CU_add_test(pSuite[suiteI], CoreTests[testI].TestName, CoreTests[testI].TestFunc) ) {
+			CU_cleanup_registry();
+			return CU_get_error();
+		}
+	}
+	suiteI++;
+	for ( testI = 0; testI < sizeof(TrapTests)/sizeof(TrapTests[0]); testI++ ) {
+		if ( !CU_add_test(pSuite[suiteI], TrapTests[testI].TestName, TrapTests[testI].TestFunc) ) {
+			CU_cleanup_registry();
+			return CU_get_error();
+		}
+	}
+	/*suiteI++;*/ /* Don't forget to uncomment this before adding more tests */
+
+	/* Run all tests using the CUnit Basic interface */
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	failCount = CU_get_number_of_failure_records();
+	CU_cleanup_registry();
+	err = CU_get_error();
+	if ( err != CUE_SUCCESS ) return err;
+	if ( failCount ) return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}
+/*-------------------------------------------------------------------------*/
+static void suite_core_Ver()
+{
+	//CU_ASSERT_STRING_EQUAL(GWFEATURES_VERSION, gwfeatures_Version());
+}
+/*-------------------------------------------------------------------------*/
+static void suite_trap_InstallTrapCallback()
+{
+	//CU_ASSERT_STRING_EQUAL(GWFEATURES_VERSION, gwfeatures_Version());
+}
+/*-------------------------------------------------------------------------*/
