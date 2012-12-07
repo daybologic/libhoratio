4cb4f8a066bb4806368f2dad48313b4710a271f2
David Duncan Ross Palmer <palmer@overchat.org>
2012-12-07 00:25:51 +0000

    Remove some trailing whitespace from dpc_stats.c

--- dpc_stats.c
+++ dpc_stats.c
@@ -8,11 +8,11 @@ modification, are permitted provided that the following conditions are met:
 
     * Redistributions of source code must retain the above copyright notice,
       this list of conditions and the following disclaimer.
-      
+
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-      
+
     * Neither the name of the Daybo Logic nor the names of its contributors
       may be used to endorse or promote products derived from this software
       without specific prior written permission.
@@ -124,7 +124,7 @@ static void CountFlagsInUse(
     /* First reset the counts in the stats struct */
     PFlagsStats->Blocks.Locked = 0UL;
     PFlagsStats->Blocks.Unswappable = 0UL;
- 
+
     /* Go through normal arrays */
     for ( i = 0U; i < DPCRTLMM_SAFETYLIST_MAXSIZE; i++ )
     {
