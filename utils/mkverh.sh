395a0dcf0f7a352fb28d69cad9fe3bcd55b3679a
David Duncan Ross Palmer <palmer@overchat.org>
2016-02-18 22:49:55 +0000

    Script to dynamically grok the version information and write to hversion.h

---
+++ utils/mkverh.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+OUTPUT='hversion.h'
+LOG='debian/changelog'
+
+ident=`utils/hgident`
+
+version=`awk -F'[()]' '{print $2; count++; if (count==2) exit}' $LOG`
+
+echo "#define HORATIO_VERSION_IDENT \"$ident\"" > $OUTPUT
+echo "#define HORATIO_VERSION \"$version\"" >> $OUTPUT
+
+set `echo $version | tr "." "\n"`
+
+printf "#define HORATIO_VERSION_MAJOR (%d)\n" "$1" >> $OUTPUT
+printf "#define HORATIO_VERSION_MINOR (%d)\n" "$2" >> $OUTPUT
+printf "#define HORATIO_VERSION_PATCH (%d)\n" "$3" >> $OUTPUT
+
+exit 0;
