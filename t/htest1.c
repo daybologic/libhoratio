115dc5849112ae5939ea70057176c727b6cfff88
David Duncan Ross Palmer <palmer@overchat.org>
2016-02-26 18:23:07 +0000

    Correct name back to 66f8569ff17e49c8dbc069251d5310d1e923e8b3

--- t/htest1.c
+++ t/htest1.c
@@ -268,7 +268,7 @@ int main(int argc, char *argv[]) {
 		const char *TestName;
 		void (*TestFunc)(void);
 	} DbghookTests[] = {
-		{ "InstallDebugHook",
+		{ "DbghookInstallDebugHook",
 		  &suite_dbghook_InstallDebugHook
 		}
 	};
