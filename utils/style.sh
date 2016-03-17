6e00dc2395fa54d7084b04e25c91ca59d9c2deab
David Duncan Ross Palmer <david.palmer@gradwell.com>
2016-03-17 19:55:12 +0000

    A load more reformatting options; seems legit to me

--- utils/style.sh
+++ utils/style.sh
@@ -3,7 +3,20 @@
 set -e
 
 for source in lib/*.[ch]; do
-	astyle --indent-switches --indent=force-tab=8 --style=attach "$source";
+	astyle \
+		--indent-switches \
+		--indent=force-tab=8 \
+		--style=attach \
+		--align-pointer=name \
+		--add-brackets \
+		--max-code-length=78 \
+		--keep-one-line-statements \
+		--break-blocks=all \
+		--indent-classes \
+		--lineend=linux \
+		--unpad-paren \
+		--pad-header \
+		"$source";
 done
 
 exit 0
